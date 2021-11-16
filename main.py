fed9ac20c98fa92feccaf00592b3154a2e4e79bd
v_ytaoliu <v_ytaoliu@tencent.com>
2021-11-16 19:53:43 +0800

    docs: init the main.py and .gitignore file

---
+++ main.py
@@ -0,0 +1,17 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset', type=str, help='Provide the dataset name (MUTAG or Enzymes)',
+                            choices=['MUTAG', 'ENZYMES'])
+    parser.add_argument('--crossvalidation', default=False, action='store_true', help='Enable a 10-fold crossvalidation')
+    parser.add_argument('--gridsearch', default=False, action='store_true', help='Enable grid search')
+    parser.add_argument('--sinkhorn', default=False, action='store_true', help='Use sinkhorn approximation')
+    parser.add_argument('--h', type = int, required=False, default=2, help = "(Max) number of WL iterations")
+
+    args = parser.parse_args()
+    dataset = args.dataset
+    print(dataset)
+
+if __name__ == "__main__":
+    main()
