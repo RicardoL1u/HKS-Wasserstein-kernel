80cc67bc505114a61e998e5791a82bd4a296865f
RicardoL1u <2018212874@bupt.edu.cn>
2022-02-21 21:03:21 +0800

    feature:baselines corpus system

--- baselines/short_path.py
+++ baselines/short_path.py
@@ -7,6 +7,7 @@ Script makes use of :class:`grakel.ShortestPath`
 """
 from __future__ import print_function
 print(__doc__)
+import argparse
 
 import numpy as np
 import sklearn.model_selection
@@ -18,28 +19,37 @@ from sklearn.metrics import accuracy_score
 from grakel.datasets import fetch_dataset
 from grakel.kernels import ShortestPath
 
-# Loads the MUTAG dataset
-MUTAG = fetch_dataset("MUTAG", verbose=False)
-G, y = MUTAG.data, MUTAG.target
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset', type=str, help='Provide the dataset name (MUTAG or PTC_FM)',
+                            choices=['MUTAG', 'PTC_FM'])
+    args = parser.parse_args()
+      
+    # Loads the MUTAG dataset
+    MUTAG = fetch_dataset(args.dataset, verbose=False)
+    G, y = MUTAG.data, MUTAG.target
 
-gk = ShortestPath(normalize=True)
-cv = sklearn.model_selection.StratifiedKFold(n_splits=10,shuffle=True)
+    gk = ShortestPath(normalize=True)
+    cv = sklearn.model_selection.StratifiedKFold(n_splits=10,shuffle=True)
 
-accuracy_scores = []
-for train_index, test_index in cv.split(G, y):
-    G_train = [G[i] for i in train_index]
-    G_test  = [G[i] for i in test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    K_train = gk.fit_transform(G_train)
-    K_test = gk.transform(G_test)
-    # Uses the SVM classifier to perform classification
-    clf = SVC(kernel="precomputed")
-    clf.fit(K_train, y_train)
-    y_pred = clf.predict(K_test)
-    accuracy_scores.append(sklearn.metrics.accuracy_score(y_test, y_pred))
+    accuracy_scores = []
+    for train_index, test_index in cv.split(G, y):
+        G_train = [G[i] for i in train_index]
+        G_test  = [G[i] for i in test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        K_train = gk.fit_transform(G_train)
+        K_test = gk.transform(G_test)
+        # Uses the SVM classifier to perform classification
+        clf = SVC(kernel="precomputed")
+        clf.fit(K_train, y_train)
+        y_pred = clf.predict(K_test)
+        accuracy_scores.append(sklearn.metrics.accuracy_score(y_test, y_pred))
 
 
-# Computes and prints the classification accuracy
-print('Mean 10-fold accuracy: {:2.2f} +- {:2.2f} %'.format(
-                    np.mean(accuracy_scores) * 100,  
-                    np.std(accuracy_scores) * 100))
+    # Computes and prints the classification accuracy
+    print('Mean 10-fold accuracy: {:2.2f} +- {:2.2f} %'.format(
+                        np.mean(accuracy_scores) * 100,  
+                        np.std(accuracy_scores) * 100))
+
+if __name__ == "__main__":
+    main()
