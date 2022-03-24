84fb9dce773b84a07b137ab38c26041b10947b18
RicardoL1u <2018212874@bupt.edu.cn>
2022-03-24 17:39:16 +0800

    feature: able to specifiy output file path

--- exp/fig1:w_versue_hl/fig.py
+++ exp/fig1:w_versue_hl/fig.py
@@ -1,11 +1,11 @@
-import csv
+# import csv
 import os
 import numpy as np
-Path = "./results/PTC_MR/WKS"
-result_files = [open(Path+i,'r') for i in os.listdir(Path)]
-result_readers = [csv.reader(f) for f in result_files]
-for r in result_readers:
-    next(r)
-
+import pandas as pd
 x_axis = np.arange(0.30,0.75,0.05).tolist()
 
+Path = "./results/PTC_MR/WKS/"
+# Get list of all files in a given directory sorted by name
+list_of_files = sorted( filter( lambda x: os.path.isfile(os.path.join(Path, x)),
+                        os.listdir(Path) ) )
+result_files = [pd.read_csv(Path+i) for i in list_of_files]
