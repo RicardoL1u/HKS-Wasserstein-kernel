30aff7a7307810e7af80232b24d58f0d3c8e95ba
RicardoL1u <2018212874@bupt.edu.cn>
2022-03-24 19:33:51 +0800

    exp:scripts

--- exp/fig1:w_versue_hl/fig.py
+++ exp/fig1:w_versue_hl/fig.py
@@ -2,23 +2,39 @@
 import os
 import numpy as np
 import pandas as pd
-x_axis = np.arange(0.30,0.75,0.05).tolist()
-
-Path = "./results/PTC_MR/WKS/"
-# Get list of all files in a given directory sorted by name
-list_of_files = sorted( filter( lambda x: os.path.isfile(os.path.join(Path, x)),
-                        os.listdir(Path) ) )
-result_files = [pd.read_csv(Path+i) for i in list_of_files]
-means = np.array([np.mean(file["accuracy"]) for file in result_files])
-stds = np.array([np.std(file["accuracy"]) for file in result_files])
+import argparse
 import matplotlib.pyplot as plt
-plt.style.use('_mpl-gallery')
-# plot
-fig, ax = plt.subplots()
-fig.set_size_inches(4, 4)
-ax.fill_between(x_axis, means-stds, means+stds, alpha=.5, linewidth=0)
-ax.plot(x_axis, means, linewidth=2)
-ax.set(xlim=(0.25, 0.75), xticks=np.arange(0.25, 0.80, 0.05),
-       ylim=(0.6, 0.7), yticks=np.arange(0.5, 0.75, 0.025))
-# plt.show()
-plt.savefig("fig1:varied_W.png", bbox_inches='tight', pad_inches=0)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset', type=str, help='Provide the dataset name',
+                            choices=['MUTAG','PTC_MR','ENZYMES'])
+    parser.add_argument('-m', '--method', type=str, help='Provide the signature name',
+                            choices=['HKS','WKS'])
+    parser.add_argument('-s','--start', type = float, required=True, help = "start value of x")
+    parser.add_argument('-e','--end', type = float, required=True, help = "end value of x")
+    parser.add_argument('-step','--step', type = float, required=True, help = "end value of x")
+    args = parser.parse_args()
+
+    x_axis = np.arange(args.start,args.end,args.step).tolist()
+    Path = "./"+args.dataset+"/"+args.method+"/"
+    # Get list of all files in a given directory sorted by name
+    list_of_files = sorted( filter( lambda x: os.path.isfile(os.path.join(Path, x)),
+                            os.listdir(Path) ) )
+    result_files = [pd.read_csv(Path+i) for i in list_of_files]
+    means = np.array([np.mean(file["accuracy"]) for file in result_files])
+    stds = np.array([np.std(file["accuracy"]) for file in result_files])
+    print(means,stds)
+    plt.style.use('_mpl-gallery')
+    # plot
+    fig, ax = plt.subplots()
+    fig.set_size_inches(4, 4)
+    ax.fill_between(x_axis, means-stds, means+stds, alpha=.5, linewidth=0)
+    ax.plot(x_axis, means, linewidth=2)
+    ax.set(xlim=(args.start-args.step, args.end), xticks=x_axis,
+        ylim=(0.55, 0.75), yticks=np.arange(0.55, 0.75, 0.025))
+    plt.show()
+    plt.savefig("fig1:varied_w_"+args.dataset+".png", bbox_inches='tight', pad_inches=0)
+
+if __name__ == "__main__":
+    main()
--- exp/fig1:w_versue_hl/fig.sh
+++ exp/fig1:w_versue_hl/fig.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
+
+#SBATCH --job-name=test
+#SBATCH --partition=v5_192
+#SBATCH -N 1
+#SBATCH --mail-type=all
+#SBATCH --mail-user=2018212874@bupt.edu.cn
+#SBATCH --output=%j.out
+#SBATCH --error=%j.err
+
+source activate myenv
+
 # bash的登号是严格不能有空格的
 method="WKS"
+method_num=1
 dataset="MUTAG"
+rm -r $dataset
 path="exp/fig1:w_versue_hl"
-method_num=0
-if method=="WKS"; then
-    method_num=1
-fi
-echo $method_num
+start=100
+end=500
+step=100
+
+
+# -------------
 pwd
 cd ../..
 pwd
 
-for ((i=1; i<=2; i++))
+for ((i=$start; i<=$end; i=i+$step))
 do
-    declare -i hl=$i*100
-    # echo $hl
-    python3 main.py -d $dataset -m $method_num -s 0 -cv -w 0.45 -hl $hl -p $path 
+    python3 main.py -d $dataset -m $method_num -s 0 -cv -w 0.45 -hl $i -p $path 
 done
+
+cd $path
+python3 fig.py -d $dataset -m $method -s $start -e $end -step $step
