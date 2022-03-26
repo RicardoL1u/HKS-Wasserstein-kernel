1c6ad71fd5a701e64428ac87ee01943b2a67915e
RicardoL1u <2018212874@bupt.edu.cn>
2022-03-26 22:10:37 +0800

    exp: enzymes w search

--- exp/fig1:w_versue_hl/exp_script.py
+++ exp/fig1:w_versue_hl/exp_script.py
@@ -3,12 +3,13 @@ import numpy as np
 import subprocess
 import time
 import shutil
-method="WKS"
-dataset="PTC_MR"
-resultzip = f'{method}_{dataset}_w_hl=1000.zip'
+method="HKS"
+sample=2
+dataset="ENZYMES"
+resultzip = f'{method}{sample}_{dataset}_w_hl=800.zip'
 start=0.0
 end=1.00
-step=0.05
+step=0.1
 
 # ===============================
 shutil.rmtree(os.path.join(dataset,method),ignore_errors=True)
@@ -26,12 +27,11 @@ os.system("pwd")
 sub_procs = []
 for w in np.arange(start,end+step,step):
     w = np.round(w,2)
-    sub_procs.append(subprocess.Popen(["python3","main.py","-d",dataset,"-m",f'{method_num}',"-w",f'{w}',"-hl",'1000','-p',output_path,'-n',f'{w}','-cv']))
+    name = '{:1.2f}'.format(w)
+    # print(name)
+    os.system(f'python3 main.py -d {dataset} -m {method_num} -s {sample} -w {w} -hl 800 -p {output_path} -n {name}  -cv')
+    # sub_procs.append(subprocess.Popen(["python3 main.py -d",dataset,"-m",f'{method_num} -s',f'{sample}',"-w",f'{w}',"-hl",'800 -p',output_path,'-n {:1.2f}'.format(w),'-cv']))
 
-for p in sub_procs:
-    while p.poll() == None:
-        time.sleep(1)
-    print("sleep over")
 
 os.chdir(now_path)
 # os.system(f'python3 fig.py -d {dataset} -m {method} -s {start} -e {end} -step {step}')
