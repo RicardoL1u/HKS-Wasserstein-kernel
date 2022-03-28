b867a372a36b73dadf3352483a3c5e4fbd665e1e
RicardoL1u <2018212874@bupt.edu.cn>
2022-03-28 21:13:33 +0800

    exp:record for WKS0_MUTAG_w_c=100.0_g=1.0_hl=800.zip

--- exp/fig1:w_versue_hl/exp_script.py
+++ exp/fig1:w_versue_hl/exp_script.py
@@ -1,18 +1,18 @@
 import os
 import numpy as np
 import shutil
-method="HKS"
-sample=2
-dataset="DD"
-C = 1000.0
-g = 0.1
+method="WKS"
+sample=0
+dataset="MUTAG"
+C = 100.0
+g = 1.0
 hl = 800
-sinkhorn = '--sinkhorn'
+sinkhorn = ''
 resultzip = f'{method}{sample}_{dataset}_w_c={C}_g={g}_hl={hl}{sinkhorn}.zip'
 start=0.4
 end=0.6
 step=0.1
-
+ws = [0.3,0.4,0.45,0.50,0.55,0.60,0.70]
 # ===============================
 shutil.rmtree(os.path.join(dataset,method),ignore_errors=True)
 os.makedirs(os.path.join(dataset,method))
@@ -27,7 +27,7 @@ os.chdir("../..")
 os.system("pwd")
 
 sub_procs = []
-for w in np.arange(start,end+step,step):
+for w in ws:
     w = np.round(w,2)
     name = '{:1.2f}'.format(w)
     os.system(f'python3 main.py -d {dataset} -m {method_num} -s {sample} -c {C} -g {g} -w {w} -hl {hl} -p {output_path} -n {name} -cv {sinkhorn}')
