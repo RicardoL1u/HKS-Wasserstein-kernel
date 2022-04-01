a8fb0a6a55003490e513efec4c917873169f19e0
RicardoL1u <2018212874@bupt.edu.cn>
2022-04-01 20:44:34 +0800

    refactor: add time records

--- exp/time/time.py
+++ exp/time/time.py
@@ -41,15 +41,27 @@ def seconds_to_format(times):
     float_time = f'{np.round(seconds-int(seconds),decimals=2)}'[1:]
         # print(float_time)
     mean = datetime.fromtimestamp(int(seconds)) - datetime.strptime("1970-01-01 08:00:00",'%Y-%m-%d %I:%M:%S')
+    
     print('Mean Time: {}{} +- {:2.2f} sec'.format(
                             mean,
                             float_time,  
                             np.std(times)))
+    return ['{}{}'.format(mean,float_time),np.std(times)]
+
+column_list = ['dataset','method','node emb mean','node emb std','data num','wass distance mean','wass dsitance std','data num']
+data = []
 for dataset in datasets:
     for method in methods:
         file = dataset+'_'+method+'.log'
         print(file)
         wass_time,node_time = stat_file(file)
-        seconds_to_format(node_time)
-        seconds_to_format(wass_time)
+        data_unit = [dataset,method]
+        data_unit.extend(seconds_to_format(node_time))
+        data_unit.append(len(node_time))
+        data_unit.extend(seconds_to_format(wass_time))
+        data_unit.append(len(wass_time))
+        data.append(data_unit)
+import pandas as pd
+df = pd.DataFrame(data,columns = column_list)
+df.to_csv('time.csv')
         
