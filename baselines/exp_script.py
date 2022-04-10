30a70fdf44f049e855f55d910a73fcbca62149fb
RicardoL1u <2018212874@bupt.edu.cn>
2022-04-10 23:13:59 +0800

    exp: MUTAG and PTC baselines

--- baselines/exp_script.py
+++ baselines/exp_script.py
@@ -1,13 +1,14 @@
 import os
 
-dataset = 'PTC_MR'
+datasets = ['MUTAG','PTC_MR']
 
 seeds = [
     # 7,
-    77,
+    # 77,
     777,
     4396,
     1205
 ]
-for seed in seeds:
-    os.system(f'python3 baselines.py -d {dataset} -gs  --seed {seed}')
+for dataset in datasets:
+    for seed in seeds:
+        os.system(f'python3 baselines.py -d {dataset} -gs  --seed {seed}')
