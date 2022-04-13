e2320c2588919cfc4d8389c644e284a7ebc6a4fc
RicardoL1u <2018212874@bupt.edu.cn>
2022-04-13 17:03:54 +0800

    feature: bench mark system

--- wass_dis.py
+++ wass_dis.py
@@ -14,8 +14,8 @@ def pairwise_wasserstein_distance(X,T,signature_method,sample_method,weight:list
     """
     
     # Embed the nodes
-    logging.basicConfig(format='%(asctime)s: %(message)s',datefmt='%Y/%m/%d %I:%M:%S',level=logging.DEBUG)
-    logging.info("ready to generate node embeddings")
+    # logging.basicConfig(format='%(asctime)s: %(message)s',datefmt='%Y/%m/%d %I:%M:%S',level=logging.DEBUG)
+    # logging.info("ready to generate node embeddings")
     node_signature_matrice = signature.CalculateSignature4Graphs(X,signature_method,sample_method,T)
     node_attr_matrice = signature.GraphAttrMatrice(X)
     logging.info("the node embedding have been generated")
@@ -25,11 +25,11 @@ def pairwise_wasserstein_distance(X,T,signature_method,sample_method,weight:list
         graph_node_embeddings = []
         for i in range(len(X)):
             graph_node_embeddings.append(np.concatenate((w*node_signature_matrice[i],(1-w)*node_attr_matrice[i]),axis=1))
-        msg = f"the wass diss with sinkhorn = {sinkhorn} and w = {w}"
-        logging.debug("ready to compute "+msg )
+        # msg = f"the wass diss with sinkhorn = {sinkhorn} and w = {w}"
+        # logging.debug("ready to compute "+msg )
         wasserstein_distances.append(_compute_wasserstein_distance(graph_node_embeddings, 
             sinkhorn=sinkhorn, sinkhorn_lambda=10))
-        logging.debug("have computed "+msg)
+        # logging.debug("have computed "+msg)
     return wasserstein_distances
 
 
